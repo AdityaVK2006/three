d77ec15d6bb60abf25ce67774c7c90672b4600b7
AdityaVK2006 <adityakatepallewar@gmail.com>
2024-09-09 17:49:53 +0530

    ar enabbled

--- main.js
+++ main.js
@@ -1,23 +1,22 @@
 import * as THREE from 'three';
-import { OrbitControls } from 'https://cdn.jsdelivr.net/npm/three@0.168.0/examples/jsm/controls/OrbitControls.js';
+import { ARButton } from 'https://cdn.jsdelivr.net/npm/three@0.168.0/examples/jsm/webxr/ARButton.js';
 
-const renderer = new THREE.WebGLRenderer();
+const renderer = new THREE.WebGLRenderer({antialias: true, alpha: true });
 renderer.setSize(window.innerWidth,window.innerHeight);
 document.body.appendChild( renderer.domElement );
+document.body.appendChild( ARButton.createButton( renderer ) );
+renderer.xr.enabled = true;
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight,0.1,1000);
-const controls = new OrbitControls(camera, renderer.domElement);
 camera.position.z = 5;
-controls.update();
-const helper = new THREE.AxesHelper(3);
-scene.add(helper);
+const helper = new THREE.AxesHelper(1);
+
 
 const cube = new THREE.Mesh(new THREE.BoxGeometry(1,1,1), new THREE.MeshBasicMaterial({color:"green"}));
+cube.position.z = -5;
 scene.add(cube);
 
-function animate(){
-    renderer.render(scene, camera);
-}
-
-renderer.setAnimationLoop(animate);
+renderer.setAnimationLoop(function(){
+	renderer.render( scene, camera );
+});
